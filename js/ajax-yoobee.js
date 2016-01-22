5959cabfb1cc29e6cf8b380af23762f7c07bf01e
Jasana Mael <jasanaemily@hotmail.com>
2016-01-22 15:23:27 +1300

    Use ajax to connect to yoobee.net

--- js/ajax-yoobee.js
+++ js/ajax-yoobee.js
@@ -1,1 +1,29 @@
-alert('hi');
+$(document).ready(function(){
+	
+	// Listen to the form
+	$('#note-form').submit()(function( e ){
+
+		// Prevent form from submitting
+		e.preventDefault();
+
+		// Grab the note
+		if( $.trim( $('#note').val() ) == '' ){
+			return;
+		}
+
+		// Prepre AJAX
+		$.ajax({
+			url: 'http://jasana.mael.yoobee.net.nz/ajax-yoobee/ajax-yoobee.php',
+			data: { note: $('#note').val() },
+			success: function( dataFromServer ) {
+
+			},
+			error: function() {
+				alert("Cannot connect");
+			}
+		});
+
+
+	});
+
+});
