05282a6d1a695abd8725e7fdf56b7f6473856ca3
Jasana Mael <jasanaemily@hotmail.com>
2016-01-26 12:28:38 +1300

    show response from server in console

--- js/ajax-yoobee.js
+++ js/ajax-yoobee.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-	
+
 	// Listen to the form
 	$('#note-form').submit(function( e ){
 
-		// Prevent form from submitting
+		// Prevent the form from submitting
 		e.preventDefault();
 
 		// Grab the note
@@ -11,18 +11,18 @@ $(document).ready(function(){
 			return;
 		}
 
-		// Prepre AJAX
+		// Prepare AJAX
 		$.ajax({
 			url: 'http://jasana.mael.yoobee.net.nz/ajax-yoobee/ajax-yoobee.php',
 			data: { note: $('#note').val() },
-			success: function( dataFromServer ) {
-
+			success: function( dataFromServer ){ 
+				console.log( dataFromServer );
+				
 			},
 			error: function() {
-				alert("Cannot connect");
+				alert('Cannot connect');
 			}
 		});
-
 
 	});
 
